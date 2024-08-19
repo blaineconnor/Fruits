023fba0c2d0f9077e057940df0c8ce633e9dd32c
fsarman <fsarman@ahlatciteknoloji.com.tr>
2024-08-19 08:14:56 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Fatih Emre Sarman on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
